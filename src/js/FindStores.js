09504fe73d062b067c926c40f95d133dde472905
LAC <lac@lee-anne-clarke.com>
2018-03-07 18:31:05 -0500

    CSS updates
    - Replaced all 'em' and 'px' with 'rem'
    - Find a Store: Added missing text

--- src/js/FindStores.js
+++ src/js/FindStores.js
@@ -48,6 +48,7 @@ class FindStores extends Component {
 					<ul className="findstores-list u-clearfix">
 						<li className="findstores-li" ref={(r) => { this.listItem1 = r; }}>
 							<b>The Fresh Grocer #1</b><br /><br />
+							123 Avenue Lane<br />
 							Philadelphia, Pennsylvania 19122<br /><br />
 							Phone: (215) 555-5555<br /><br />
 							<a className="btn" href="#">Driving Directions</a>
